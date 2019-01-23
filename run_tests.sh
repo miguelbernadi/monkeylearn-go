244b26665cdb18e2523241fee375a662180152f3
Miguel Bernabeu <miguel.bernabeu@devex.com>
2019-01-23 23:53:11 +0100

    Add scripts to aid benchmarking
    Use `run_test.sh` to define which tests to run and in which order and
    store the execution results, including the results of `time` command.
    
    `total_seq_ms.sh` processes a logfile from an execution and aggregates
    all the duration messages to estimate the runtime of a purely
    sequential implementation.

---
+++ run_tests.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -eu
+
+TOKEN=${TOKEN:-}
+CLASSIFIER=${CLASSIFIER:-}
+
+run() {
+    LOGFILE=results_${INPUT_FILE}_${BATCH_SIZE}_${RPM}.log
+    { time go run ./cmd/ \
+       -token $TOKEN \
+       -classifier $CLASSIFIER \
+       -batch $BATCH_SIZE \
+       -rpm $RPM \
+       -file ${INPUT_FILE}.json \
+    2> ${LOGFILE} ; } 2>> ${LOGFILE}
+}
+
+# Process 50 documents
+INPUT_FILE=articles_50 BATCH_SIZE=1 RPM=120 run
+INPUT_FILE=articles_50 BATCH_SIZE=2 RPM=120 run
+INPUT_FILE=articles_50 BATCH_SIZE=5 RPM=120 run
+INPUT_FILE=articles_50 BATCH_SIZE=10 RPM=120 run
+INPUT_FILE=articles_50 BATCH_SIZE=25 RPM=120 run
+INPUT_FILE=articles_50 BATCH_SIZE=50 RPM=120 run
+
+# Process 100 documents
+INPUT_FILE=articles_100 BATCH_SIZE=1 RPM=120 run
+INPUT_FILE=articles_100 BATCH_SIZE=2 RPM=120 run
+INPUT_FILE=articles_100 BATCH_SIZE=5 RPM=120 run
+INPUT_FILE=articles_100 BATCH_SIZE=10 RPM=120 run
+INPUT_FILE=articles_100 BATCH_SIZE=20 RPM=120 run
+INPUT_FILE=articles_100 BATCH_SIZE=25 RPM=120 run
+INPUT_FILE=articles_100 BATCH_SIZE=50 RPM=120 run
+INPUT_FILE=articles_100 BATCH_SIZE=100 RPM=120 run
+
+# Process 200 documents
+INPUT_FILE=articles_200 BATCH_SIZE=1 RPM=120 run
+INPUT_FILE=articles_200 BATCH_SIZE=2 RPM=120 run
+INPUT_FILE=articles_200 BATCH_SIZE=5 RPM=120 run
+INPUT_FILE=articles_200 BATCH_SIZE=10 RPM=120 run
+INPUT_FILE=articles_200 BATCH_SIZE=20 RPM=120 run
+INPUT_FILE=articles_200 BATCH_SIZE=25 RPM=120 run
+INPUT_FILE=articles_200 BATCH_SIZE=50 RPM=120 run
+INPUT_FILE=articles_200 BATCH_SIZE=100 RPM=120 run
+INPUT_FILE=articles_200 BATCH_SIZE=200 RPM=120 run
